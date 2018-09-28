003c130ea0d598508408f12b836cd52a4e163f4b
Mckarl Manuel <manmckarl@gmail.com>
2018-09-28 12:26:50 -1000

    complete

--- js/nested-for-loops.js
+++ js/nested-for-loops.js
@@ -7,8 +7,18 @@
 function nestedForLoops (depth, width = depth) {
   let result = "\n";
 
-  // Write code here
-
+  for (let i = 0; i < depth; i++) {
+    for (let j = 0; j < width; j++) {
+      result += `{x:${j}, y:${i}}`
+      if (j < width - 1) {
+        result += `, `
+      } 
+      if (j === width - 1) {
+        result += "\n"
+      }
+    }
+  }
+  return result
 };
 
 // To see your console output outside the tests add function calls here.
